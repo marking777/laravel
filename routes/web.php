88bffb6f563fd4c826e965dc591fbabf310db7a0
Marouane el Jaghnouni <2179336@talnet.nl>
2023-10-17 11:23:29 +0200

    added route

--- routes/web.php
+++ routes/web.php
@@ -18,3 +18,9 @@ Route::get('/planeten', function () {
     $planeten = ["Uranus", "Jupiter", "Mars", "Aarde", "Saturnus", "Pluto", "Neptunus", "Venus"];
     return response()->json($planeten);
 });
+
+
+Route::get('/test', function () {
+    $planeten = ["Uranus", "Jupiter", "Mars", "Aarde", "Saturnus", "Pluto", "Neptunus", "Venus"];
+    return response()->json($planeten);
+});
