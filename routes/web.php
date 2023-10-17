3a0e866f50899604029932a42500e916398f96c0
Marouane el Jaghnouni <2179336@talnet.nl>
2023-10-17 11:37:36 +0200

    add

--- routes/web.php
+++ routes/web.php
@@ -24,3 +24,27 @@ Route::get('/test', function () {
     $planeten = ["Uranus", "Jupiter", "Mars", "Aarde", "Saturnus", "Pluto", "Neptunus", "Venus"];
     return response()->json($planeten);
 });
+use Illuminate\Support\Facades\View;
+
+Route::get('/planets', function () {
+    $planets = [
+        [
+            'name' => 'Mars',
+            'description' => 'Mars is the fourth planet from the Sun and the second-smallest planet in the Solar System, being larger than only Mercury.'
+        ],
+        [
+            'name' => 'Venus',
+            'description' => 'Venus is the second planet from the Sun. It is named after the Roman goddess of love and beauty.'
+        ],
+        [
+            'name' => 'Earth',
+            'description' => 'Our home planet is the third planet from the Sun, and the only place we know of so far that\'s inhabited by living things.'
+        ],
+        [
+            'name' => 'Jupiter',
+            'description' => 'Jupiter is a gas giant and doesn\'t have a solid surface, but it may have a solid inner core about the size of Earth.'
+        ],
+    ];
+
+    return View::make('planets')->with('planets', $planets);
+});
